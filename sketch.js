a0e4e54cd65d86f7d0784734148d88fbf337063c
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-02-05 11:32:05 -0600

    Refactor sketch.js for code consistency and clarity

--- sketch.js
+++ sketch.js
@@ -1,17 +1,25 @@
-  let myInput;
+let myInput;
+let theirNumber;
 
 function setup() {
   createCanvas(800, 400);
   //frameRate(1)
   myInput = createInput();
-  myInput.position(100,100);
-  myInput.size(200,50);
+  myInput.position(100, 100);
+  myInput.size(200, 50);
+  theirNumber = 0;
+}
+function draw() {
+  background("#3A86FF");
+  //textSize(72)
+  text("My First Project", 50, 75);
+  textSize(72);
+  //text("My First Project",75,150);
+  getNumber();
 }
 
-function draw(){ 
-  background('#3A86FF');
-  //textSize(72)
-  text("My First Project",50,75);
-  textSize(72)
-  //text("My First Project",75,150);
-}
+function getNumber() {
+  theirNumber = myInput.value()
+  console.log(theirNumber)
+} //end getNumber
+
