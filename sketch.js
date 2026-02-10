c5912eae42ede2a42764d9691b53fd6c36bd573a
corbin Boughen <50217007+theBough@users.noreply.github.com>
2026-02-10 11:29:12 -0600

    Enhance input fields and load custom font
    Added input for number of digits and updated input sizes.

--- sketch.js
+++ sketch.js
@@ -1,19 +1,33 @@
 let myInput;
+let howManyDigit;
 let theirNumber;
+let myFont;
 
 function setup() {
   createCanvas(800, 400);
-  //frameRate(1)
+  
+  //create an input to get the original number
   myInput = createInput();
-  myInput.position(100, 100);
-  myInput.size(200, 50);
-  theirNumber = 0;
+  myInput.position(225, 100);
+   myInput.size(100, 25);
+  
+  //this input will be for how many digits the user wants to keep
+  howManyDigit = createInput()
+  howManyDigit.position(375,155)
+  howManyDigit.size(25,25)
+  
+  //load a custum font
+  myFont = loadFont("PlaywriteCUGuides-Regular.ttf")  
 }
 function draw() {
   background("#3A86FF");
-  text("My First Project", 50, 75);
-  textSize(72);
-  text("Your number is: " + getNumber(myInput.value(),3), 100, 220);
+  textFont(myFont)
+  text("Playing with Numbers", 50, 75);
+  text("Enter a number:", 50, 125);
+  text("How many digits do you want:", 50, 175);
+  
+  textSize(24);
+  text("Your number is: " + getNumber(myInput.value(),howManyDigit.value()), 100, 220);
 }
 
 function getNumber(thisNum,howManyDigits) {
